f620927774787da25d0d9cd1df9dfec501d3114e
RogerCasalsVilardell <43968704+RogerCasalsVilardell@users.noreply.github.com>
2020-03-02 11:52:39 +0100

    Detector cases added to task 2

--- week1.py
+++ week1.py
@@ -102,8 +102,11 @@ def task1():
 
 
 def task2():
-    reader = AICityChallengeAnnotationReader(path='data/ai_challenge_s03_c010-full_annotation.xml')
+        reader = AICityChallengeAnnotationReader(path='data/ai_challenge_s03_c010-full_annotation.xml')
     gt = reader.get_annotations(classes=['car'], group_by_frame=True, boxes_only=True)
+
+    # Noisy case
+
     noise_params = {'drop': 0.05, 'mean': 0, 'std': 10}
     gt_noisy = reader.get_annotations(classes=['car'], noise_params=noise_params, group_by_frame=True, boxes_only=True)
 
@@ -144,6 +147,54 @@ def task2():
     ax[1].set_ylabel('mean IoU')
     fig.suptitle('noisy annotation')
     plt.show()
+
+    # Detector cases
+
+    detectors = ['det_mask_rcnn.txt', 'det_ssd512.txt', 'det_yolo3.txt']
+    detections_path = 'data/AICity_data/train/S03/c010/det/'
+
+    for detector in detectors:
+        print("Detector: ", detector)
+        dets_reader = AICityChallengeAnnotationReader(path=detections_path + detector)
+        detections_list = dets_reader.get_annotations(classes=['car'], group_by_frame=True, boxes_only=True)
+
+        frames = sorted(list(set(gt) & set(detections_list)))
+        overlaps = []
+        for frame in frames:
+            boxes1 = [box[1:] for box in gt[frame]]
+            boxes2 = [box[1:] for box in detections_list[frame]]
+            iou = mean_intersection_over_union(boxes1, boxes2)
+            overlaps.append(iou)
+
+        cap = cv2.VideoCapture('data/AICity_data/train/S03/c010/vdo.avi')
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+
+        fig, ax = plt.subplots(2)
+        image = ax[0].imshow(np.zeros((height, width)))
+        line, = ax[1].plot(frames, overlaps)
+        artists = [image, line]
+
+        def update(i):
+            cap.set(cv2.CAP_PROP_POS_FRAMES, frames[i])
+            ret, img = cap.read()
+            for box in gt[frames[i]]:
+                cv2.rectangle(img, (int(box[1]), int(box[2])), (int(box[3]), int(box[4])), (0, 255, 0), 2)
+            for box in detections_list[frames[i]]:
+                cv2.rectangle(img, (int(box[1]), int(box[2])), (int(box[3]), int(box[4])), (0, 0, 255), 2)
+            artists[0].set_data(img[:, :, ::-1])
+            artists[1].set_data(frames[:i+1], overlaps[:i+1])
+            return artists
+
+        ani = animation.FuncAnimation(fig, update, len(frames), interval=2, blit=True)
+
+        ax[0].set_xticks([])
+        ax[0].set_yticks([])
+        ax[1].set_ylim(0, 1)
+        ax[1].set_xlabel('#frame')
+        ax[1].set_ylabel('mean IoU')
+        fig.suptitle('Detector '+detector.replace('.txt',''))
+        plt.show()
 
 
 def task3_4():
