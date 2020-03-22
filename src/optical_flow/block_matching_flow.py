cf62b345455369ceec5e874ab1abeb3157e58f12
Oscar Mañas <oscmansan@gmail.com>
2020-03-22 19:10:25 +0100

    Add more block matching algorithms

--- src/optical_flow/block_matching_flow.py
+++ src/optical_flow/block_matching_flow.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from skimage.feature import match_template
 from tqdm import trange
 
 
@@ -19,22 +20,51 @@ def distance(x1: np.ndarray, x2: np.ndarray, metric='euclidean'):
 
 
 def block_matching(reference, target, metric='euclidean', algorithm='es'):
+    """
+    Search reference in target and return the position with maximum correlation
+    """
     if algorithm == 'es':
+        # exhaustive search
         min_dist = np.inf
-        disp = (0, 0)
+        pos = (0, 0)
         for i in range(target.shape[0]-reference.shape[0]):
             for j in range(target.shape[1]-reference.shape[1]):
                 dist = distance(reference, target[i:i+reference.shape[0], j:j+reference.shape[1]], metric)
                 if dist < min_dist:
-                    disp = (i, j)
+                    pos = (i, j)
                     min_dist = dist
-        return disp
+        return pos
+    elif algorithm == 'tss':
+        # three step search
+        step = 8
+        orig = ((target.shape[0]-reference.shape[0])//2, (target.shape[1]-reference.shape[1])//2)
+        step = (min(step, orig[0]), min(step, orig[1]))
+        while step[0] > 1 and step[1] > 1:
+            min_dist = np.inf
+            pos = orig
+            for i in [orig[0]-step[0], orig[0], orig[0]+step[0]]:
+                for j in [orig[1]-step[1], orig[1], orig[1]+step[1]]:
+                    dist = distance(reference, target[i:i + reference.shape[0], j:j + reference.shape[1]], metric)
+                    if dist < min_dist:
+                        pos = (i, j)
+                        min_dist = dist
+            orig = pos
+            step = (step[0]//2, step[1]//2)
+        return orig
+    elif algorithm == 'corr':
+        # normalized correlation
+        corr = match_template(target, reference)
+        pos = np.unravel_index(np.argmax(corr), corr.shape)
+        return pos
     else:
         raise ValueError(f'Unknown block matching algorithm: {algorithm}')
 
 
 def block_matching_flow(img_prev: np.ndarray, img_next: np.ndarray, block_size=16, search_area=7,
                         motion_type='backward', metric='euclidean', algorithm='es'):
+    """
+    Compute block-matching based motion estimation
+    """
     if motion_type == 'forward':
         reference = img_prev
         target = img_next
@@ -105,7 +135,7 @@ if __name__ == '__main__':
     img_next = cv2.imread('../../data/data_stereo_flow/training/image_0/000045_11.png', cv2.IMREAD_GRAYSCALE)
     flow_noc = read_flow('../../data/data_stereo_flow/training/flow_noc/000045_10.png')
 
-    flow = block_matching_flow(img_prev, img_next)
+    flow = block_matching_flow(img_prev, img_next, algorithm='corr')
     #flow = cv2.calcOpticalFlowFarneback(img_prev, img_next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
 
     err = np.sqrt(np.sum((flow_noc[..., :2] - flow) ** 2, axis=2))
