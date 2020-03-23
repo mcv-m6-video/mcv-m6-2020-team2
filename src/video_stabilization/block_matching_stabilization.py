57d7aab1e2388766d24c35d353b714902c0b55ef
RogerCasalsVilardell <43968704+RogerCasalsVilardell@users.noreply.github.com>
2020-03-23 15:13:20 +0100

    Improved method for 2_1

--- src/video_stabilization/block_matching_stabilization.py
+++ src/video_stabilization/block_matching_stabilization.py
@@ -2,17 +2,39 @@ import numpy as np
 import cv2
 import imageio
 from tqdm import trange
+from scipy.signal import medfilt
 
 from src.optical_flow.block_matching_flow import block_matching_flow
 
 
+def unit_vector(vector):
+    return vector / np.linalg.norm(vector)
+
+def angle_between(v1, v2):
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+
 def apply_camera_motion(frame, optical_flow, w, h, acc_t, method='average'):
 
     if method == 'average':
-        average_optical_flow = np.array(optical_flow.mean(axis=0).mean(axis=0), dtype=int)
+        # Average
+        average_optical_flow = - np.array(optical_flow.mean(axis=0).mean(axis=0), dtype=np.float32)
         acc_t += average_optical_flow
         H = np.float32([[1, 0, acc_t[0]], [0, 1, acc_t[1]]])
-        frame_stabilized = cv2.warpAffine(frame, H, (w, h))   
+        frame_stabilized = cv2.warpAffine(frame, H, (w, h))
+
+    if method == 'median_average':
+        # Median
+        optical_flow = optical_flow.flatten().reshape(h * w, 2)
+        np.random.shuffle(optical_flow)
+        optical_flow[:,0] = medfilt(optical_flow[:,0], 5)
+        optical_flow[:,1] = medfilt(optical_flow[:,1], 5)
+        # Average
+        average_optical_flow = - np.array(optical_flow.mean(axis=0), dtype=np.float32)
+        acc_t += average_optical_flow
+        H = np.float32([[1, 0, acc_t[0]], [0, 1, acc_t[1]]])
+        frame_stabilized = cv2.warpAffine(frame, H, (w, h))
 
     else:
         raise ValueError('Camera motion method not valid.')
@@ -25,10 +47,12 @@ def block_matching_stabilization(cap, output_file, to_video, video_percentage=1)
     h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = int(cap.get(cv2.CAP_PROP_FPS))
 
-    w = 400
-    h = 250
-    method = 'average'
+    # Resize frames for computational speed
+    w = 600
+    h = 350
+    method = 'median_average'
 
+    # Stabilize video sequence
     if to_video:
         out = cv2.VideoWriter(f'{output_file}/block_matching_stabilization_'+method+'.mp4', cv2.VideoWriter_fourcc(*'XVID'), fps, (w, h))
     else:
@@ -38,9 +62,10 @@ def block_matching_stabilization(cap, output_file, to_video, video_percentage=1)
     end = int(n_frames * video_percentage)
     previous_frame = None
     acc_t = np.zeros(2)
+    acc_list = []
     for i in trange(start, end, desc="Apply stabilization and saving video"):
         success, frame = cap.read()
-        frame = cv2.resize(frame, (w, h))
+        frame = cv2.resize(frame, (w, h), interpolation=cv2.INTER_AREA)
         if not success:
             break
 
@@ -48,12 +73,13 @@ def block_matching_stabilization(cap, output_file, to_video, video_percentage=1)
             frame_stabilized = frame
         else:
             # Compute optical flow between frames
-            optical_flow = block_matching_flow(previous_frame, frame)
+            optical_flow = block_matching_flow(previous_frame, frame, block_size=32, search_area=16, motion_type='forward')
 
             # Stabilize image
             frame_stabilized, acc_t = apply_camera_motion(frame, optical_flow, w, h, acc_t, method=method)
 
         previous_frame = frame
+        acc_list.append(acc_t)
 
         if to_video:
             out.write(frame_stabilized)
