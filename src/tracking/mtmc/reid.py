ef8877275bce3accb9b15315fedaad984716a69f
Oscar Mañas <oscmansan@gmail.com>
2020-04-12 00:15:32 +0200

    Cluster match candidates for a track

--- src/tracking/mtmc/reid.py
+++ src/tracking/mtmc/reid.py
@@ -4,71 +4,12 @@ import pprint
 
 import numpy as np
 import cv2
-from sklearn.cluster import MeanShift
-from sklearn.metrics.pairwise import paired_distances
+from sklearn.cluster import DBSCAN
 from tqdm import tqdm
 
 from utils.aicity_reader import parse_annotations_from_txt, group_by_frame, group_by_id
 from tracking.mtmc.encoder import Encoder
 from tracking.mtmc.camera import read_calibration, read_timestamps, angle_to_cam, bbox2gps, time_range, angle
-
-
-def reid_exhaustive(root, width, height, batch_size):
-    encoder = Encoder()
-    encoder = encoder.cuda()
-    encoder.eval()
-
-    tracks = {}
-    for camera in ['c010', 'c011', 'c012', 'c013', 'c014', 'c015']:
-        camera_path = os.path.join(root, camera)
-        detections = group_by_frame(parse_annotations_from_txt(os.path.join(camera_path, 'mtsc', 'mtsc_tc_mask_rcnn.txt')))
-        cap = cv2.VideoCapture(os.path.join(camera_path, 'vdo.avi'))
-
-        # process detections frame by frame
-        track_embeddings = defaultdict(list)
-        batch = []
-        ids = []
-        for frame in tqdm(detections.keys(), desc=f'cam {camera}'):
-            # read frame
-            cap.set(cv2.CAP_PROP_POS_FRAMES, frame)
-            ret, img = cap.read()
-
-            # crop and resize detections
-            for det in detections[frame]:
-                if det.width >= width and det.height >= height:
-                    img_cropped = img[int(det.ytl):int(det.ybr), int(det.xtl):int(det.xbr)]
-                    if img_cropped.size > 0:
-                        img_resized = cv2.resize(img_cropped, (width, height))
-                        batch.append(img_resized)
-                        ids.append(det.id)
-
-            # compute embeddings if enough detections in batch
-            if len(batch) >= batch_size:
-                embeddings = encoder.get_embeddings(batch)
-                for id, embd in zip(ids, embeddings):
-                    track_embeddings[id].append(embd)
-                batch.clear()
-                ids.clear()
-
-        # compute embeddings of last batch
-        if len(batch) > 0:
-            embeddings = encoder.get_embeddings(batch)
-            for id, embd in zip(ids, embeddings):
-                track_embeddings[id].append(embd)
-
-        # combine embeddings of each track
-        for id, embeddings in track_embeddings.items():
-            tracks[(camera, id)] = np.stack(embeddings).mean(axis=0)
-
-    # cluster embeddings to associate tracks
-    ids = list(tracks.keys())
-    embeddings = list(tracks.values())
-    ms = MeanShift()
-    ms.fit(np.stack(embeddings))
-    clusters = defaultdict(list)
-    for id, label in zip(ids, ms.labels_):
-        clusters[label].append(id)
-    pprint.pprint(clusters)
 
 
 def is_static(track, thresh=50):
@@ -141,6 +82,35 @@ def get_track_embeddings(tracks_by_cam, cap, encoder, batch_size=512):
     return embeddings
 
 
+def reid_exhaustive(root):
+    cams = set(os.listdir(root))
+
+    # read data
+    tracks_by_cam = {cam: group_by_id(parse_annotations_from_txt(os.path.join(root, cam, 'mtsc', 'mtsc_tc_mask_rcnn.txt'))) for cam in cams}
+    cap = {cam: cv2.VideoCapture(os.path.join(root, cam, 'vdo.avi')) for cam in cams}
+
+    # filter out static tracks
+    for cam in cams:
+        tracks_by_cam[cam] = dict(filter(lambda x: not is_static(x[1]), tracks_by_cam[cam].items()))
+
+    # initialize encoder
+    encoder = Encoder()
+    encoder = encoder.cuda()
+    encoder.eval()
+
+    # compute all embeddings
+    embeddings = get_track_embeddings(tracks_by_cam, cap, encoder)
+    embeddings = {(cam, id): embd for cam in embeddings for id, embd in embeddings[cam].items()}
+
+    # cluster embeddings to associate tracks
+    clustering = DBSCAN(eps=3, min_samples=2)
+    clustering.fit(np.stack(list(embeddings.values())))
+    clusters = defaultdict(list)
+    for id, label in zip(embeddings.keys(), clustering.labels_):
+        clusters[label].append(id)
+    pprint.pprint(clusters)
+
+
 def reid_spatiotemporal(root, seq, metric='euclidean', thresh=10):
     seq_path = os.path.join(root, 'train', seq)
     cams = set(os.listdir(seq_path))
@@ -161,7 +131,7 @@ def reid_spatiotemporal(root, seq, metric='euclidean', thresh=10):
     encoder = encoder.cuda()
     encoder.eval()
 
-    # precompute all embeddings
+    # compute all embeddings
     embeddings = get_track_embeddings(tracks_by_cam, cap, encoder)
 
     matches = []
@@ -171,6 +141,7 @@ def reid_spatiotemporal(root, seq, metric='euclidean', thresh=10):
             dir1 = bbox2gps(track1[-1].bbox, H[cam1]) - bbox2gps(track1[-min(int(fps[cam1]), len(track1)-1)].bbox, H[cam1])
             range1 = time_range(track1, timestamp[cam1], fps[cam1])
 
+            candidates = []
             for cam2 in cams-{cam1}:
                 if angle_to_cam(track1, H[cam1], cam2) < 45:  # going towards the camera
                     for id2, track2 in tracks_by_cam[cam2].items():
@@ -180,15 +151,17 @@ def reid_spatiotemporal(root, seq, metric='euclidean', thresh=10):
 
                         if range2[0] >= range1[0]:  # car is detected later in second camera
                             if angle(dir1, dir2) < 15:  # tracks have similar direction
-                                embd1 = embeddings[cam1][id1]
-                                embd2 = embeddings[cam2][id2]
-                                dist = paired_distances([embd1], [embd2], metric)[0]
-                                if dist < thresh:  # TODO: improve this
-                                    matches.append(((cam1, id1), (cam2, id2)))
+                                candidates.append((cam2, id2))
 
+            clustering = DBSCAN(eps=3, min_samples=2)  # TODO: choose appropriate eps
+            clustering.fit(np.stack([embeddings[cam][id] for cam, id in [(cam1, id1)] + candidates]))
+            label1 = clustering.labels_[0]
+            for (cam2, id2), label2 in zip(candidates, clustering.labels_[1:]):
+                if label2 == label1:
+                    matches.append(((cam1, id1), (cam2, id2)))
     print(matches)
 
 
 if __name__ == '__main__':
-    # reid_exhaustive('../../../data/AIC20_track3/train/S03', width=128, height=128, batch_size=512)
+    # reid_exhaustive('../../../data/AIC20_track3/train/S03')
     reid_spatiotemporal('../../../data/AIC20_track3', 'S03')
