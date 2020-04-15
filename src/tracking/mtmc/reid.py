cc761cc96711a4afe5a716a436bd35fca49925ac
María Gil Aragones <maria.gilaragones@gmail.com>
2020-04-15 20:23:32 +0200

    Add together all reid methods

--- src/tracking/mtmc/reid.py
+++ src/tracking/mtmc/reid.py
@@ -6,12 +6,14 @@ from collections import defaultdict
 import cv2
 import numpy as np
 from sklearn.cluster import DBSCAN
+from sklearn.metrics.pairwise import pairwise_distances
 from tqdm import tqdm
 import networkx as nx
 
 from evaluation.idf1 import MOTAcumulator
 from tracking.mtmc.camera import read_calibration, read_timestamps, angle_to_cam, bbox2gps, time_range, angle
 from tracking.mtmc.encoder import Encoder
+from tracking.track import Track
 from utils.aicity_reader import parse_annotations_from_txt, group_by_frame, group_by_id, group_in_tracks
 
 
@@ -120,7 +122,97 @@ def reid_exhaustive(root):
     pprint.pprint(clusters)
 
 
-def reid_spatiotemporal(root, seq, model_path, metric='euclidean'):
+def reid_spatiotemporal(root, seq, model_path, metric='euclidean', thresh=10):
+    seq_path = os.path.join(root, 'train', seq)
+    cams = sorted([f for f in os.listdir(seq_path) if not f.startswith('.')])
+
+    # read data
+    tracks_by_cam = {cam: group_in_tracks(parse_annotations_from_txt(os.path.join(seq_path, cam, 'mtsc', 'mtsc_tc_mask_rcnn.txt')), cam) for cam in cams}
+    cap = {cam: cv2.VideoCapture(os.path.join(seq_path, cam, 'vdo.avi')) for cam in cams}
+    fps = {cam: cap[cam].get(cv2.CAP_PROP_FPS) for cam in cams}
+    H = {cam: read_calibration(os.path.join(seq_path, cam, 'calibration.txt')) for cam in cams}
+    timestamp = read_timestamps(os.path.join(root, 'cam_timestamp', f'{seq}.txt'))
+
+    # filter out static tracks
+    for cam in cams:
+        tracks_by_cam[cam] = dict(filter(lambda x: not is_static(x[1].get_track()), tracks_by_cam[cam].items()))
+
+    # initialize encoder
+    encoder = Encoder(path=model_path)
+    encoder.eval()
+
+    # compute all embeddings
+    embeddings_file = f'./embeddings/{seq}.pkl'
+    if os.path.exists(embeddings_file):
+        with open(embeddings_file, 'rb') as f:
+            embeddings = pickle.load(f)
+    else:
+        embeddings = get_track_embeddings(tracks_by_cam, cap, encoder, save_path=embeddings_file)
+
+    for cam1 in cams:
+        for id1, track1 in tracks_by_cam[cam1].items():
+            dets1 = track1.get_track()
+            dets1.sort(key=lambda det: det.frame)
+            dir1 = bbox2gps(dets1[-1].bbox, H[cam1]) - bbox2gps(dets1[-min(int(fps[cam1]), len(dets1) - 1)].bbox,
+                                                                H[cam1])
+            range1 = time_range(dets1, timestamp[cam1], fps[cam1])
+
+            candidates = []
+            for cam2 in cams:
+                if cam2 == cam1:
+                    continue
+
+                if angle_to_cam(dets1, H[cam1], cam2) < 45:  # going towards the camera
+                    for id2, track2 in tracks_by_cam[cam2].items():
+                        dets2 = track2.get_track()
+                        dets2.sort(key=lambda det: det.frame)
+                        dir2 = bbox2gps(dets2[min(int(fps[cam2]), len(dets2) - 1)].bbox, H[cam2]) - bbox2gps(
+                            dets2[0].bbox, H[cam2])
+                        range2 = time_range(dets2, timestamp[cam2], fps[cam2])
+
+                        if range2[0] >= range1[0]:  # car is detected later in second camera
+                            if angle(dir1, dir2) < 15:  # tracks have similar direction
+                                if not track2.get_prev_track() and not track1.get_next_track():
+                                    # track has not been previously matched to another track from the same direction
+                                    candidates.append((cam2, id2))
+
+            if len(candidates) > 0:
+                dist = pairwise_distances([embeddings[cam1][id1]],
+                                          [embeddings[cam2][id2] for cam2, id2 in candidates],
+                                          metric).flatten()
+                ind = dist.argmin()
+                if dist[ind] < thresh:
+                    # merge matched tracks
+                    cam2, id2 = candidates[ind]
+                    tracks_by_cam[cam1][id1].set_next_track((cam2, id2))
+                    tracks_by_cam[cam2][id2].set_prev_track((cam1, id1))
+
+    starting_tracks = []
+    for cam, tracks in tracks_by_cam.items():
+        for id, track in tracks.items():
+            if track.get_next_track() and not track.get_prev_track():
+                starting_tracks.append(track)
+
+    # propagate ids through tracks connected to starting tracks
+    results = defaultdict(dict)
+    track_count = 1
+    for track in starting_tracks:
+        track.id = track_count
+        results[track.camera][track_count] = track
+        next_track = track.get_next_track()
+
+        while next_track:
+            cam_next, id_next = next_track
+            track_to_propagate = tracks_by_cam[cam_next][id_next]
+            track_to_propagate.id = track_count
+            results[track_to_propagate.camera][track_count] = track_to_propagate
+            next_track = track_to_propagate.get_next_track()
+        track_count += 1
+
+    return results
+
+
+def reid_spatiotemporal_graph(root, seq, model_path, metric='euclidean'):
     seq_path = os.path.join(root, 'train', seq)
     cams = sorted([f for f in os.listdir(seq_path) if not f.startswith('.')])
 
@@ -200,6 +292,8 @@ def write_results(tracks_by_cam, path):
     for cam, tracks in tracks_by_cam.items():
         lines = []
         for track in tracks:
+            if isinstance(track, Track):
+                track = track.get_track()
             for det in track:
                 lines.append((det.frame, det.id, int(det.xtl), int(det.ytl), int(det.width), int(det.height),
                               det.score, '-1', '-1', '-1'))
@@ -211,12 +305,20 @@ def write_results(tracks_by_cam, path):
             for line in lines:
                 file.write(','.join(list(map(str, line))) + '\n')
 
+def reid(root, seq, model_path, reid_method):
+    if reid_method == 'exhaustive':
+        return reid_exhaustive(root)
+    elif reid_method == 'spatio_temporal_consec':
+        return reid_spatiotemporal(root, seq, model_path)
+    elif reid_method == 'spatio_temporal_graph':
+        return reid_spatiotemporal_graph(root, seq, model_path)
+
 
 if __name__ == '__main__':
     root = '../../../data/AIC20_track3'
     seq = 'S03'
 
-    results = reid_spatiotemporal(root, seq, model_path='../metric_learning/checkpoints/epoch_19__ckpt.pth')
+    results = reid_spatiotemporal_graph(root, seq, model_path='../metric_learning/checkpoints/epoch_19__ckpt.pth')
     write_results(results, path=os.path.join('results', seq))
 
     accumulator = MOTAcumulator()
